1f6d4ffc693ec3fab1b147aa459a2195089e3aa5
Burning-Bear <zpcxh95@outlook.com>
2022-02-17 16:47:49 +0800

    add tag

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='RLA',
-        version=0.4,
+        version="0.4.1",
         description=(
             'RL assistant'
         ),
