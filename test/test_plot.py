a5f88dd609a3031faaedb5895192eb0cd7de5236
Xiong-Hui Chen <chenxh@lamda.nju.edu.cn>
2022-08-11 11:57:07 +0800

    test(plot): add user cases

--- test/test_plot.py
+++ test/test_plot.py
@@ -1,6 +1,7 @@
 # Created by xionghuichen at 2022/8/10
 # Email: chenxh@lamda.nju.edu.cn
 from test._base import BaseTest
+import numpy as np
 from RLA.easy_log.log_tools import DeleteLogTool, Filter
 from RLA.easy_log.log_tools import ArchiveLogTool, ViewLogTool
 from RLA.easy_log.tester import exp_manager
@@ -71,3 +72,14 @@ class ScriptTest(BaseTest):
                       key_to_legend_fn=my_key_to_legend,
                       shaded_range=False, pretty=True, show_number=False)
 
+    def test_post_process(self):
+        data_root, task = self.get_basic_info()
+        regs = [
+            '2022/03/01/21-[12]*'
+        ]
+
+        _ = plot_func(data_root=data_root, task_table_name=task, regs=regs, split_keys=['learning_rate'],
+                      metrics=['perf/mse'],
+                      scale_dict={'perf/mse': lambda x: np.log(x)},
+                      ylabel='RMSE',
+                      shaded_range=False, pretty=True, show_number=False)
