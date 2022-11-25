a5424707832f9164f21675eea70c472f1e58d291
xionghuichen <chenxh@lamda.nju.edu.cn>
2022-11-25 03:21:19 +0000

    Add time tracker

--- RLA/easy_log/time_used_recorder.py
+++ RLA/easy_log/time_used_recorder.py
@@ -3,9 +3,78 @@
 from RLA.easy_log import logger
 import time
 
+class SingleTimeTracker:
+    def __init__(self,name:str='untitled') -> None:
+        self.name=name
+        self.t = 0.0
+        self.call_time=0
+        self.time_cost = 0.0
+
+    def __enter__(self):
+        # trace time
+        self.call_time+=1            
+        self.t = time.perf_counter()
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.time_cost += time.perf_counter() - self.t
+
+
+class TimeTracker:
+    def __init__(self):
+        self.t0=time.time()#to calc total time
+        self.time_dict=dict()
+
+    def add(self,name='untitled'):
+        """
+        :param name: specify the SingleTimeTracker in the time_dict, recommend use
+        line num in the scripts, like 'xxx.py Line xxx', can be easily got in python scripts using 
+        `os.path.basename(__file__)+'  line '+str(sys._getframe().f_lineno)`  
+        """
+        if name not in self.time_dict.keys():
+            self.time_dict.update({name:SingleTimeTracker(name)})
+        return self.time_dict[name]
+
+    def __call__(self, name:str):
+        return self.time_dict[name]
+        
+    def clear(self):
+        self.time_dict=dict()
+
+    def statistic_entry(self,name:str):
+        """
+        calc total calls of the 
+        """
+        assert name in self.time_dict.keys()
+
+        t1=time.time()
+        t_passed=t1-self.t0
+        return {
+            'total calls/'+name:self.time_dict[name].call_time,
+            'total time cost/'+name:self.time_dict[name].time_cost,
+            'average time cost/'+name:self.time_dict[name].time_cost/(1e-6+self.time_dict[name].call_time),
+            'time cost percentage/'+name:self.time_dict[name].time_cost/(1e-6+t_passed)
+        }
+
+    def get_info(self):
+        info={}
+        for k in self.time_dict.keys():
+            for entry_k,entry_v in self.statistic_entry(k).items():
+                info[entry_k]=entry_v
+        return info
+
+    def log(self,exclude_lst=['csv']):
+        logger.info('---------time dashboard---------')
+        for k in self.time_dict.keys():
+            for entry_k,entry_v in self.statistic_entry(k).items():
+                logger.record_tabular('time_used/'+entry_k,entry_v,exclude=exclude_lst)
+                logger.info(f"[{entry_k}]: {entry_v}")
+            logger.info('')
+        logger.info('---------dashboard end---------')
+
 
 rc_start_time = {}
-
 
 def time_record(name):
     """
